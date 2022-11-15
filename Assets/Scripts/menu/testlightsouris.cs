58e36f82e3f793c374374201425a9086ec247ea5
Tayuuu <manon.talbot@e-artsup.net>
2022-11-15 11:59:35 +0100

    light in menu et script light

---
+++ Assets/Scripts/menu/testlightsouris.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
+
+public class testlightsouris : MonoBehaviour
+{
+    public GameObject Light;
+    Vector3 posMouse;
+
+
+    public void Update()
+    {
+        Light.transform.GetComponent<Camera>().enabled = true;
+    }
+
+    public void FixedUpdate()
+    {
+        posMouse = Input.mousePosition;
+        Vector3 pos = Camera.main.ScreenToWorldPoint(posMouse);
+        pos.z = 0;
+        transform.position = pos;
+    }
+}
