ca6810f03b5d55fc900223e2dbb6fe5df80fe1dc
Léo <leolecointre50@gmail.com>
2022-11-16 09:46:59 +0100

    dash directionnel

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,23 +9,24 @@ public class Player : MonoBehaviour
     private bool canDash = true;
     private bool isDashing;
     private float dashingPower = 24f;
+    private float dashingCooldown = 1f;
     private float dashingTime = 0.15f;
-    private float dashingCooldown = 1.5f;
     private float originalGravity;
+    [SerializeField] private TrailRenderer tr;
 
-    public float speed = .0f; 
-    int MaxNumberOfJumps = 1;
-    public int NumberOfJumps = 0;
-    public float JumpForce = 9.0f;
-    new SpriteRenderer renderer = null;
-    public HeadRotation headRotation;
-    public GameObject head;
+    public float speed = .0f;
     public bool isReverse;
-
+    new SpriteRenderer renderer = null;
     Rigidbody2D rb = null;
     //Animator animator = null;
     Vector2 movement = Vector2.zero;
-    [SerializeField] private TrailRenderer tr;
+
+    int MaxNumberOfJumps = 1;
+    public int NumberOfJumps = 0;
+    public float JumpForce = 9.0f;
+
+    public HeadRotation headRotation;
+    public GameObject head;
 
     void Start(){      
         rb = GetComponent<Rigidbody2D>();
@@ -46,13 +47,20 @@ public class Player : MonoBehaviour
         if (movement.x != 0)
         {
             renderer.flipX = movement.x < 0;
-            isReverse = true;
+            isReverse = movement.x < 0;
         }
     }
 
     public void OnDash(InputValue dashValue){
         if (canDash){
-            StartCoroutine(Dash());
+            if (isReverse){
+                dashingPower = -dashingPower;
+                StartCoroutine(Dash());
+                dashingPower = -dashingPower;
+            }
+            else{
+                StartCoroutine(Dash());
+            }
         }
     }
 
@@ -85,9 +93,8 @@ public class Player : MonoBehaviour
     private IEnumerator Dash(){
         canDash = false;
         isDashing = true;
-        float dashGravity = 0f;
 
-        rb.gravityScale = dashGravity;
+        rb.gravityScale = 0f;
         rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
         tr.emitting = true;
         yield return new WaitForSeconds(dashingTime);
