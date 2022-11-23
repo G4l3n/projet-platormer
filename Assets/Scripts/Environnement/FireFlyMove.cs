a718e1889c57bd77ec6e306f916e66406d6c6aec
Léo <leolecointre50@gmail.com>
2022-11-23 12:15:07 +0100

    Lucioles qui fonctionnent et maj du prefab player

--- Assets/Scripts/Environnement/FireFlyMove.cs
+++ Assets/Scripts/Environnement/FireFlyMove.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class FireflyMove : MonoBehaviour
 {
     public float moveSpeed = 5.8f;
+    float sinCenterY;
+    public Vector2 direction;
     void Start()
     {
-        
+        sinCenterY = transform.position.y;
+        direction.Normalize();
     }
 
     private void FixedUpdate()
     {
-        Vector2 pos = transform.position;
+        float x = moveSpeed * Time.fixedDeltaTime;
 
-        pos.x -= moveSpeed * Time.fixedDeltaTime;
+        float y = Mathf.Sin(Time.realtimeSinceStartup * 6) * 0.1f;
 
-        transform.position = pos;
+        Vector2 delta = direction * x + y * -Vector2.Perpendicular(direction);
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
     }
 }
