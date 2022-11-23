2b3e0c8617f4fddc77c8993ea0b6880f4eb56a97
Tayuuu <manon.talbot@e-artsup.net>
2022-11-23 10:06:49 +0100

    creation scene lvl et script loadlevel

--- Assets/Scripts/menu/ScriptMenu.cs
+++ Assets/Scripts/menu/ScriptMenu.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScriptMenu : MonoBehaviour
 {
-    public GameObject Button;
+    
 
     public void QuitGame()
     {
         Debug.Log("quit");
         Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("credits");
     }
 
     public void Update()
---
+++ Assets/Scripts/menu/ScriptSelectionChap.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptSelectionChap : MonoBehaviour
+{
+   public void LoadLevel1()
+   {
+        SceneManager.LoadScene("lvl1");
+   }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("lvl2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("lvl3");
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("lvl");
+    }
+
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene("lvl5");
+    }
+
+    public void LoadLevel6()
+    {
+        SceneManager.LoadScene("lvl6");
+    }
+
+    public void LoadLevel7()
+    {
+        SceneManager.LoadScene("lvl7");
+    }
+
+    public void LoadLevel8()
+    {
+        SceneManager.LoadScene("lvl8");
+    }
+}
