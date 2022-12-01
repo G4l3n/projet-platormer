5b0bc0bd98b055d478b56d1941fd6c8b4c2ce825
G4l3n <ethan.le.bigot18@gmail.com>
2022-12-01 09:22:41 +0100

    debut menu pause

--- Assets/Scripts/menu/Pause.cs
+++ Assets/Scripts/menu/Pause.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class echap : MonoBehaviour
+public class Pause : MonoBehaviour
 {
     public GameObject pauseMenu;
 
