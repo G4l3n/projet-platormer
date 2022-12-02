956ed1e52512cf7606d568988b52fd7132df78fd
Tayuuu <manon.talbot@e-artsup.net>
2022-12-02 12:22:49 +0100

    compteur de mort fini

--- Assets/Scripts/Environnement/Kill.cs
+++ Assets/Scripts/Environnement/Kill.cs
@@ -1,30 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Kill : MonoBehaviour
 {
-    public bool Death = false;
+
+    public int NbreDeMort;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+        if (PlayerPrefs.GetInt("NbreDeMort") != 0)
+        {
+            NbreDeMort = PlayerPrefs.GetInt("NbreDeMort");
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Death = true;
-            Debug.Log("died");
+            NbreDeMort++;
+            PlayerPrefs.SetInt("NbreDeMort", NbreDeMort);
+            Debug.Log(NbreDeMort);
             Destroy(other.gameObject);
+            SceneManager.LoadScene("Manon");
         } 
     }
 }
