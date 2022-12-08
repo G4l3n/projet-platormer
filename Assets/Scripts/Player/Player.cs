5a30fa9a9645166e22298d0668f2258eac96524a
Léo <leolecointre50@gmail.com>
2022-12-08 12:07:24 +0100

    nouveau système de saut et wallslide

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour
     public GameObject head;
 
     [Header("Movement")]
+    public bool isMoving = false;
     public float speed = 0f;
     Vector2 movement = Vector2.zero;
 
@@ -20,11 +21,20 @@ public class Player : MonoBehaviour
     public bool canJump = false;
     public float jumpForce = 9.0f;
     private float originalGravity;
+    public float rayLenght =1;
 
     [Header("Dash")]
     public Dash Dash;
 
     [Header("Wall Jump")]
+    public LayerMask groundLayer;
+    public bool isGrounded = false;
+    public float wallJumpTime = 0.2f;
+    public float wallSlideSpeed = 0.3f;
+    public float wallDistance = 0.5f;
+    public bool isWallSliding = false;
+    RaycastHit2D wallCheckHit;
+    private float jumpTime;
 
     [Header("Sprite")]
     new SpriteRenderer renderer = null;
@@ -55,44 +65,62 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        canJump = false;
+        canJump = Physics2D.Raycast(transform.position, -transform.up.normalized, rayLenght, groundLayer).collider != null;
+        isGrounded = canJump;
+        animator.SetBool("IsJumping", !canJump);
+
+        // Wall Jump
+
+        if (!isReverse)
+        {
+            wallCheckHit = Physics2D.Raycast(transform.position, new Vector2(wallDistance, 0), wallDistance, groundLayer);
+            Debug.DrawRay(transform.position, new Vector2(wallDistance, 0), Color.green);
+        }
+        else
+        {
+            wallCheckHit = Physics2D.Raycast(transform.position, new Vector2(-wallDistance, 0), wallDistance, groundLayer);
+            Debug.DrawRay(transform.position, new Vector2(-wallDistance, 0), Color.green);
+        }
+
+        if (wallCheckHit && !isGrounded && isMoving)
+        {
+            isWallSliding = true;
+            jumpTime = Time.time + wallJumpTime;
+        }
+        else if (jumpTime < Time.time)
+        {
+            isWallSliding = false;
+        }
+
+        if (isWallSliding)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, wallSlideSpeed, float.MaxValue));
+        }
     }
     public void OnJump(InputValue jumpValue)
     {
         float innerValue = jumpValue.Get<float>();
         if (innerValue > 0 && rb != null && canJump)
         {
-            animator.SetBool("IsJumping", true);
+            isGrounded = false;
             rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
     }
-
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        foreach(ContactPoint2D contact in collision.contacts)
-        {
-            if (contact.normal.y > 0.9f)
-            {
-                animator.SetBool("IsJumping", false);
-                canJump = true;
-            }
-        }
-    }
-
     public void OnMove(InputValue moveValue)
     {
         movement = moveValue.Get<Vector2>();
-        if (movement == Vector2.zero)
-        {
-            Debug.Log("stop");
-            GetComponent<AudioSource>().Stop();
-        }
-        else
-        {
-            Debug.Log("play");
-            GetComponent<AudioSource>().Play();
-        }
+        isMoving = true;
+        //if (movement == Vector2.zero)
+        //{
+        //    Debug.Log("stop");
+        //    GetComponent<AudioSource>().Stop();
+        //}
+        //else
+        //{
+        //    Debug.Log("play");
+        //    GetComponent<AudioSource>().Play();
+        //}
         
     }
 }
