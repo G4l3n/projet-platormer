60b1fd04339e30910245285527a6a14fe64d3640
Léo <leolecointre50@gmail.com>
2022-12-13 00:44:10 +0100

    Début de la chauve-souris, colisions, lumière en fonction des animations, début de la détection du joueur pour la réveiller

---
+++ Assets/Scripts/Environnement/Ennemies/Bat/Bat.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    public Animator animator = null;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //StartCoroutine(Test());
+    }
+    void Update()
+    {
+        
+    }
+    //IEnumerator Test()
+    //{
+    //    yield return new WaitForSeconds(3);
+    //    animator.SetBool("IsAwake", true);
+    //    yield return new WaitForSeconds(3);
+    //    animator.SetBool("IsAwake", false);
+    //    yield return new WaitForSeconds(3);
+    //    animator.SetBool("IsAwake", true);
+    //    yield return new WaitForSeconds(3);
+    //    animator.SetBool("IsFlying", true);
+    //    yield return new WaitForSeconds(3);
+    //    Debug.Log("OK");
+    //}
+}
---
+++ Assets/Scripts/Environnement/Ennemies/Bat/BatAwake.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatAwake : MonoBehaviour
+{
+    private Animator animator = null;
+    void Start()
+    {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("enter");
+            animator.SetBool("IsAwake", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("enter");
+            animator.SetBool("IsAwake", false);
+        }
+    }
+}
