95014ffb3499912b3b379a8b5c9e7999014802c8
Léo <leolecointre50@gmail.com>
2022-12-13 19:45:27 +0100

    chute de la chauve-souris fonctionnelle

--- Assets/Scripts/Environnement/Ennemies/Bat/BatOnMove.cs
+++ Assets/Scripts/Environnement/Ennemies/Bat/BatOnMove.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Drawing.Inspector.PropertyDrawers;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class BatOnMove : MonoBehaviour
@@ -12,6 +13,8 @@ public class BatOnMove : MonoBehaviour
     public Bat bat = null;
     RaycastHit2D hit;
     public LayerMask groundLayer;
+    public float fallSpeed = 1f;
+    public bool isFalling = false;
     void Start()
     {
         hit = Physics2D.Raycast(transform.position, -Vector2.up, 50f, groundLayer);
@@ -22,19 +25,24 @@ public class BatOnMove : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            //StartCoroutine(Fall());
+            StartCoroutine(Fall());
         }
     }
 
-    //IEnumerator Fall()
-    //{
-    //    rb.gravityScale = 2f;
-    //    while (transform.position.y <= hit.point.y + 2.5f)
-    //    {
-    //        transform.position = new Vector3(x + moveSpeed * Time.deltaTime, y, startPos.z);
-    //    }
-    //    rb.gravityScale = 0f;
-    //    rb.simulated = false;
-    //    animator.SetBool("IsFlying", true);
-    //}
+    private void Update()
+    {
+        if (isFalling)
+        {
+            var x = bat.transform.position.x;
+            var y = bat.transform.position.y;
+            bat.transform.position = new Vector3(x, y - fallSpeed * Time.deltaTime, bat.transform.position.z);
+        }
+    }
+    IEnumerator Fall()
+    {
+        isFalling = true;
+        yield return new WaitUntil(() => bat.transform.position.y <= hit.point.y + 2.5f);
+        isFalling = false;
+        animator.SetBool("IsFlying", true);
+    }
 }
