607522e519b257c62c180c7b28d760edd96a2f25
Léo <leolecointre50@gmail.com>
2022-12-14 10:22:15 +0100

    rien de spécial

--- Assets/Scripts/Player/Dash.cs
+++ Assets/Scripts/Player/Dash.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 public class Dash : MonoBehaviour
 {
