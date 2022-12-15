434b39d9cc85cb9da00864d244eba5262f70135b
Tayuuu <manon.talbot@e-artsup.net>
2022-12-15 15:33:12 +0100

    script gamemanager mortplayer

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -74,7 +74,7 @@ public class GameManager : MonoBehaviour
             StartCoroutine(Volume());
         }
 
-        mortplayer = GetComponent<Animator>();
+        
     }
 
     //Kill Condition
